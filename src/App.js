8371fbd13ea79dbe3aff81b1398de5d2eb3e0265
Amir <amiromayer@gmail.com>
2019-02-28 00:42:10 +0500

    Bootstrapped the main page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact
-          name="John Doe"
-          email="johndoe@gmail.com"
-          phone="000-000-00-00"
-        />
-        <Contact
-          name="Adam Smith"
-          email="asmith@gmail.com"
-          phone="000-111-00-00"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="johndoe@gmail.com"
+            phone="000-000-00-00"
+          />
+          <Contact
+            name="Adam Smith"
+            email="asmith@gmail.com"
+            phone="000-111-00-00"
+          />
+        </div>
       </div>
     );
   }
