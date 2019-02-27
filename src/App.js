cb478337dc043cc929e36ee9f7ee90920aa7d9cd
Amir <amiromayer@gmail.com>
2019-02-27 23:43:00 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <Contact
+          name="John Doe"
+          email="johndoe@gmail.com"
+          phone="000-000-00-00"
+        />
+        <Contact
+          name="Adam Smith"
+          email="asmith@gmail.com"
+          phone="000-111-00-00"
+        />
       </div>
     );
   }
