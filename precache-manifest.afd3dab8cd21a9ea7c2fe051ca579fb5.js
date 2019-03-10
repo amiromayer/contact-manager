5140f8e125da2c1d5adf5eee5aadc8385c9a2b9a
Amir <amiromayer@gmail.com>
2019-03-10 16:03:36 +0500

    Updates

---
+++ precache-manifest.afd3dab8cd21a9ea7c2fe051ca579fb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "c95b44e16c1ec60bbf92",
+    "url": "/contact-manager/static/js/main.c95b44e1.chunk.js"
+  },
+  {
+    "revision": "1110511e70281bd28a77",
+    "url": "/contact-manager/static/js/2.1110511e.chunk.js"
+  },
+  {
+    "revision": "c95b44e16c1ec60bbf92",
+    "url": "/contact-manager/static/css/main.2b4a5582.chunk.css"
+  },
+  {
+    "revision": "3710e1c45b96ab2b499f41384d8d0ae0",
+    "url": "/contact-manager/index.html"
+  }
+];
