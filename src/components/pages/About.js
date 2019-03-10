3171360157768062b65c80bc4d3374e04fa1a01b
Amir <amiromayer@gmail.com>
2019-03-10 14:55:26 +0500

    Added edit page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1 classname="mb-2">About</h1>
+      <h1 className="mb-2">About</h1>
       <p className="lead">Dummy app to manage contacts. Built with React</p>
     </div>
   );
